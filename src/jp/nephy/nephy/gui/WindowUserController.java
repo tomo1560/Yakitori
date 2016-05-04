b5a041b7e5718856d57b74c330745026ec3bcc1c
pois0 <poispois00@gmail.com>
2016-05-04 23:51:59 +0900

    Update: Show remaining word count and add a startup sound

--- src/jp/nephy/nephy/gui/WindowUserController.java
+++ src/jp/nephy/nephy/gui/WindowUserController.java
@@ -1,13 +1,18 @@
 package jp.nephy.nephy.gui;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import twitter4j.ResponseList;
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.User;
@@ -31,6 +36,12 @@ public class WindowUserController {
 	@FXML private Text window_user_text_bio;
 	@FXML private Hyperlink window_user_hyperlink_link;
 	@FXML private Button window_user_button_show_detail;
+	@FXML private Tab window_user_tab_likes;
+
+	@FXML private ListView<Status> window_user_listview_tweets;
+	@FXML private ListView<Status> window_user_listview_likes;
+	@FXML private ListView<User> window_user_listview_following;
+	@FXML private ListView<User> window_user_listview_follower;
 
 	public void setStage(Stage stage) {
 		primaryStage = stage;
@@ -90,6 +101,43 @@ public class WindowUserController {
 		window_user_text_bio.setText(user.getDescription());
 		window_user_text_bio.setWrappingWidth(width);
 		window_user_imageview_user_icon.setImage(cache.imageCache(user));
+
+		window_user_listview_tweets.setCellFactory(listview -> new CellStatus(primaryStage, twitter));
+		ObservableList<Status> list = createUserStatus();
+		window_user_listview_tweets.setItems(list);
+
 	}
 
+	private ObservableList<Status> createUserStatus() {
+		ResponseList<Status> userTimeline = null;
+		try {
+			userTimeline = twitter.getUserTimeline(userId);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return Utils.toObservableList(userTimeline);
+	}
+
+	private ObservableList<Status> createUserLikes() {
+		ResponseList<Status> userLikes = null;
+		try {
+			userLikes = twitter.getFavorites(userId);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return Utils.toObservableList(userLikes);
+	}
+
+	private ObservableList<User> createFollowing() {
+		ResponseList<User> userFollowing = null;
+		try {
+			userFollowing = twitter.getFriendsList(userId, 1);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return Utils.toObservableList(userFollowing);
+	}
 }
