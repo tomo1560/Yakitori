fdd21c0e202aa08b0e18d180f6bfa34422ad698f
Nagisberry <nagisberry@gmail.com>
2016-06-12 22:44:28 +0900

    Fix: Property name

--- Yakitori/Utility.cs
+++ Yakitori/Utility.cs
@@ -3,12 +3,12 @@ namespace Yakitori
 {
     public class Utility
     {
-        private static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
 
         public static long GetEpochTime(DateTime targetTime)
         {
             targetTime = targetTime.ToUniversalTime();
-            return (long)(targetTime - Utility.UNIX_EPOCH).TotalSeconds;
+            return (long)(targetTime - UnixEpoch).TotalSeconds;
         }
     }
 }
