aef177a69f5f8cc845c65f943209062c026cec83
Nephy <admin@nephy.jp>
2016-06-12 20:26:49 +0900

    Add: Utility

---
+++ Yakitori/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Yakitori
+{
+    public class Utility
+    {
+        private static DateTime UNIX_EPOCH = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+        public static long GetEpochTime(DateTime targetTime)
+        {
+            targetTime = targetTime.ToUniversalTime();
+            return (long)(targetTime - Utility.UNIX_EPOCH).TotalSeconds;
+        }
+    }
+}
+
