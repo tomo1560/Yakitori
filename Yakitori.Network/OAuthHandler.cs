100af29f6f95ef6ed8214f972880c25093aa8aa9
Nephy <admin@nephy.jp>
2016-06-12 20:49:22 +0900

    Add: Yakitori.Network

---
+++ Yakitori.Network/OAuthHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// using AsyncOAuth;
+namespace Yakitori
+{
+    public class OAuthHandler
+    {
+        private const string TwitterForAndroidConsumerKey = "3nVuSoBZnx6U4vzUxf5w";
+        private const string TwitterForAndroidConsumerSecret = "Bcs59EFbbsdF6Sl9Ng71smgStWEGwXXKSjYvPVt7qys";
+        private const string ConsumerKey = "8DLHQ1dsTk0ZRaaKjKsrl59Q9";
+        private const string ConsumerSecret = "9G4rmBb04S5UnyxOi392jzpMu2e12APHb9k5BLUFpFAr6KwAv4";
+
+        public OAuthHandler()
+        {
+        }
+
+        public static void Initializer()
+        {
+        }
+	}
+}
---
+++ Yakitori.Network/TwitterAPI.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Yakitori
+{
+    public class TwitterAPI
+    {
+        public TwitterAPI()
+        {
+        }
+    }
+}
+
