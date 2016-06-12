e49d7915016d4a08ca67be8898f6f7769f571f15
Nagisberry <nagisberry@gmail.com>
2016-06-12 19:16:42 +0900

    Add: PluginManager

---
+++ Yakitori.Core/PluginManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+namespace Yakitori.Core
+{
+	public class PluginManager
+	{
+		readonly DirectoryInfo PluginDirectory;
+
+		public PluginManager()
+		{
+			var current = Environment.CurrentDirectory;
+			var dir = System.IO.Path.Combine(current, "plugins");
+			if (Directory.Exists(dir))
+			{
+				PluginDirectory = new DirectoryInfo(dir);
+			}
+			else
+			{
+				PluginDirectory = Directory.CreateDirectory(dir);
+			}
+		}
+
+		public void LoadPlugins()
+		{
+			foreach (var file in PluginDirectory.GetFiles("*.dll"))
+			{
+				var asm = Assembly.LoadFrom(file.FullName);
+			}
+		}
+	}
+}
+
