f8ed32599149ddc6b920cc71e9cc925703b177ac
Nephy <admin@nephy.jp>
2016-06-13 14:27:34 +0900

    Add: CK/CSs

--- Yakitori.Network/OAuthHandler.cs
+++ Yakitori.Network/OAuthHandler.cs
@@ -8,6 +8,16 @@ namespace Yakitori.Network
         private const string TwitterForAndroidConsumerSecret = "Bcs59EFbbsdF6Sl9Ng71smgStWEGwXXKSjYvPVt7qys";
         private const string ConsumerKey = "8DLHQ1dsTk0ZRaaKjKsrl59Q9";
         private const string ConsumerSecret = "9G4rmBb04S5UnyxOi392jzpMu2e12APHb9k5BLUFpFAr6KwAv4";
+        private const string iOSConsumerKey = "6sZDw5D1mYAq7qzcxhrNc0osn";
+        private const string iOSConsumerSecret = "SDQRwRQ0IMuWt3An8XG7v7J6dcwgCUQnWuGWhAEJtPRii4dUDe";
+        private const string AndroidConsumerKey = "Rqhvt2uqYcmz3lqIDlagn3ZYq";
+        private const string AndroidConsumerSecret = "ST3FG5sSf03nVXZWamCPNHp7uk5yPjlDySBZd2qFiaB2mAWqE8";
+        private const string MacConsumerKey = "yV8lCUyTdjLhGDBcC49PS09iO";
+        private const string MacConsumerSecret = "RtQq1NapUMSoNS2La0ovUAz4xgOhOnADkvSW3oHIPOvuISELrK";
+        private const string WindowsConsumerKey = "0POp0My2jELeYsQoTlyjW119B";
+        private const string WindowsConsumerSecret = "Pa1EJduTC1nVNZzWHZvVfM6FNDUTYOL2Xx5A0GhTILDgwhWKj4";
+        private const string WindowsPhoneConsumerKey = "Thy4yRA3kQecZsMAJ3Qa2ZAhW";
+        private const string WindowsPhoneConsumerSecret = "VdOjvT8w7vT2ts0b9z7Ws96aC8LRm4nAcmahGUMmfmKn9TWIKy";
 
         public OAuthHandler()
         {
