fe0f1e0f60e3b9c33b211b2c15cb77a650dc6964
Nagisberry <nagisberry@gmail.com>
2016-06-13 20:43:58 +0900

    Add: YakitoriPluginID

---
+++ Yakitori/Core/Plugin/Attribute/YakitoriPluginID.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Yakitori.Core.Plugin.Attribute
+{
+    class YakitoriPluginID : System.Attribute
+    {
+        public readonly string PluginID;
+
+        public YakitoriPluginID(string id)
+        {
+            PluginID = id;
+        }
+    }
+}
