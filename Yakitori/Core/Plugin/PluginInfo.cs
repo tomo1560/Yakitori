ef4d7f56528bf28a235ad1c58327cea98efe322b
Nagisberry <nagisberry@gmail.com>
2016-06-13 21:43:43 +0900

    Add: PluginInfo

---
+++ Yakitori/Core/Plugin/PluginInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yakitori.Core.Plugin
+{
+    class PluginInfo
+    {
+        public readonly string MainClass;
+        public readonly string PluginId;
+        public readonly string PluginName;
+        public readonly string Description;
+        public readonly string Version;
+        public readonly string Author;
+
+        public PluginInfo(string main, string id, string name, string desc, string version, string author)
+        {
+            MainClass = main;
+            PluginId = id;
+            PluginName = name ?? "Unnamed";
+            Description = desc ?? "";
+            Version = version ?? "0.0.0";
+            Author = author ?? "Unnamed";
+        }
+    }
+}
