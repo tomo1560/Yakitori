3e38895d1cad7a036cea18c22e983c0fe84afe3e
Nagisberry <nagisberry@gmail.com>
2016-06-13 15:25:04 +0900

    Move: Yakitori.Core

---
+++ Yakitori/Core/IPlugin.cs
@@ -0,0 +1,10 @@
+﻿namespace Yakitori.Core
+{
+    public interface IPlugin
+    {
+        string PluginID { get; }
+
+        void OnEnable();
+        void OnDisable();
+    }
+}
--- Yakitori/Core/PluginManager.cs
+++ Yakitori/Core/PluginManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+
 namespace Yakitori.Core
 {
 	public class PluginManager
