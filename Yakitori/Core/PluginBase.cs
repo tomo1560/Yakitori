e3c6549e887cab1cfd0e7e715c43aad3cc2c7bd9
Nagisberry <nagisberry@gmail.com>
2016-06-13 18:25:34 +0900

    Update: PluginBase

--- Yakitori/Core/PluginBase.cs
+++ Yakitori/Core/PluginBase.cs
@@ -3,6 +3,10 @@
     public abstract class PluginBase
     {
         public abstract string PluginID { get; }
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+        public abstract string Version { get; }
+        public abstract string Author { get; }
 
         private bool _enabled = false;
 
@@ -16,38 +20,34 @@
 
         public void SetEnable()
         {
-            if (!enabled && OnEnable())
+            if (!enabled)
             {
+                OnEnable();
                 _enabled = true;
-                // ToDo: Logに有効化できた旨を表示
+                // ToDo: Logに有効化された旨を表示
             }
             else
             {
-                // ToDo: Logに有効化できなかった旨を表示
+                // ToDo: Logに有効化に失敗した旨を表示
             }
         }
 
         public void SetDisable()
         {
-            if (enabled && OnDisable())
+            if (enabled)
             {
+                OnDisable();
                 _enabled = false;
-                // ToDo: Logに無効化できた旨を表示
+                // ToDo: Logに無効化された旨を表示
             }
             else
             {
-                // ToDo: Logに無効化できなかった旨を表示
+                // ToDo: Logに無効化に失敗した旨を表示
             }
         }
 
-        private bool OnEnable()
-        {
-            return true;
-        }
+        private void OnEnable() {}
 
-        private bool OnDisable()
-        {
-            return true;
-        }
+        private void OnDisable() {}
     }
 }
