d04075b5bf989a1eb671b57c08cdd532f47c03b4
Nagisberry <nagisberry@gmail.com>
2016-06-13 20:42:17 +0900

    Fix: YakitoriPluginClass

--- Yakitori/Core/Plugin/Attribute/YakitoriPluginClass.cs
+++ Yakitori/Core/Plugin/Attribute/YakitoriPluginClass.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 
-namespace Yakitori.Core.Plugin
+namespace Yakitori.Core.Plugin.Attribute
 {
     [AttributeUsage(AttributeTargets.Assembly)]
-    class YakitoriPluginClass : Attribute
+    class YakitoriPluginClass : System.Attribute
     {
         public readonly string PluginClass;
 
