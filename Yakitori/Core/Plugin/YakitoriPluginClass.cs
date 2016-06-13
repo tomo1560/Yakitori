57e97ebcde26525cecad5398b6b3455e6b4c0e93
Nagisberry <nagisberry@gmail.com>
2016-06-13 19:35:30 +0900

    Add: YakitoriPluginClass

---
+++ Yakitori/Core/Plugin/YakitoriPluginClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Yakitori.Core.Plugin
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    class YakitoriPluginClass : Attribute
+    {
+        public readonly string PluginClass;
+
+        public YakitoriPluginClass(string clazz)
+        {
+            PluginClass = clazz;
+        }
+    }
+}
