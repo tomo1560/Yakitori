9cf93537654c46788d05dfe61b3d49cbb319eed7
Nagisberry <nagisberry@gmail.com>
2016-06-15 23:02:19 +0900

    Add: PluginBase

---
+++ YakitoriAPI/Plugin/PluginBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yakitori.API.Plugin
+{
+    public abstract class PluginBase
+    {
+        private IPluginInfo _info;
+
+        public IPluginInfo Info
+        {
+            get
+            {
+                return _info;
+            }
+        }
+
+        public void Init(IPluginInfo info)
+        {
+            _info = info;
+        }
+
+        public abstract void Initialize();
+    }
+}
