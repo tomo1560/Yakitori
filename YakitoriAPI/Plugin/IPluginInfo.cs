c48528d7843dec0603811192cf4da8a5093cb803
Nagisberry <nagisberry@gmail.com>
2016-06-15 23:02:03 +0900

    Add: PluginInfo

---
+++ YakitoriAPI/Plugin/IPluginInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace Yakitori.API.Plugin
+{
+    public interface IPluginInfo
+    {
+        string DLLFile { get; }
+        string MainClass { get; }
+        string PluginID { get; }
+        string Name { get; }
+        string Description { get; }
+        string Version { get; }
+        string[] Dependencies { get; }
+        IAuthorInfo Author { get; }
+    }
+
+    public interface IAuthorInfo
+    {
+        string Name { get; }
+        string Twitter { get; }
+        string Web { get; }
+        string email { get; }
+    }
+}
